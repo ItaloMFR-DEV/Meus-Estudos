b77cce46b2b97ea65c8f07ae70bb17d82bd1d1c0
italoMFR <italomfr.dev@gmail.com>
2022-09-21 05:40:21 -0300

    Update v1.01

--- js/config.js
+++
@@ -1,7 +0,0 @@
-const adminLogin = 'admin';
-const adminPass = 'admin';
-
-export default {
-   adminLogin: adminLogin,
-   adminPass: adminPass,
-};
--- js/index.js
+++ js/index.js
@@ -1,19 +1,19 @@
-import config from "./config";
+var adminLogin = 'admin';
+var adminPass = '123';
 
-function getLogin() {
-    playerName = document.getElementById('putLogin').value
-    playerPass = document.getElementById('putPass').value
-    if (playerName == adminLogin && playerPass == adminPass) {
+function getLogin(){
+   let playerName = document.getElementById('putLogin').value
+   let playerPass = document.getElementById('putPass').value
+   if (playerName == adminLogin && playerPass == adminPass) {
         $('msg').append("<div class='ui violet message'>Autorizado Chefe!</div>");
         setTimeout(function () {
             $('msg').append("<div class='ui active inverted dimmer'><div class='ui text loader'>Carregando</div></div><p></p>");
         }, 1000)
         setTimeout(function () {
-            $(location).attr('href', './adminpainel.html');
+            $(location).attr('href', './pages/adminpainel.html');
 
         }, 5000)
-        alert('Funcionou!')
-    } else (alert('Deu Errado!'))
+    } else (alert('Voce nao é o meu Chefe!'))
 }
 
 
